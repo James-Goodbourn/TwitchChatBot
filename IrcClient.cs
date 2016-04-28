1e66b58b5e45a8c7076c1bb97b699e9cbdee6d12
James Goodbourn <jsg5@hotmail.com>
2016-04-28 16:04:16 +0100

    add files
    as above

---
+++ IrcClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace TwitchChatReader
+{
+    class IrcClient
+    {
+        string UserName;
+        string Channel;
+
+        TcpClient client;
+        StreamReader input;
+        StreamWriter output;
+        public IrcClient(string ip, int port, string userName, string password)
+        {
+
+            UserName = userName;
+            client = new TcpClient(ip, port);
+            input = new StreamReader(client.GetStream());
+            output = new StreamWriter(client.GetStream());
+
+            output.WriteLine("PASS " + password);
+            output.WriteLine("NICK " + userName);
+            output.WriteLine("USER " + userName + " 8 * :" + userName);
+            output.Flush();
+        }
+
+        public void JoinRoom(string channel)
+        {
+            Channel = channel;
+            output.WriteLine("JOIN #" + channel);
+            output.Flush();
+        }
+
+        public void SendIRCMessage(string message)
+        {
+            output.WriteLine(message);
+            output.Flush();
+        }
+
+        public void SendChatMessage(string message)
+        {
+            SendIRCMessage(":" + UserName + "!" + 
+                UserName + "@" + UserName + 
+                ".tmi.twitch.tv PRIVMSG #" + Channel + " :" + message);
+        }
+
+        public string readMessage()
+        {
+            string message = input.ReadLine().Split('#').Last().Split(':').Last();
+            return message;
+    
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace TwitchChatReader
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        static System.Timers.Timer timer;
+        static IrcClient irc;
+
+        static bool HasInput = false;
+        static void Main(string[] args)
+        {
+            irc = new IrcClient("irc.twitch.tv", 6667, "enchebot", "oauth:rtwexvheqi1yz7aupnhuxy2cfnu3n4");
+            irc.JoinRoom("jimyfastfingers");
+
+            timer = new System.Timers.Timer(300000);
+            timer.Elapsed += timer_Tick;
+            timer.Enabled = true;
+
+            while (true)
+            {
+                try
+                {
+                    string message = irc.readMessage();
+                    if (!HasInput)
+                    {
+                        if (message != null)
+                        {
+                            Console.WriteLine(message);
+                            if (message.ToLower() == "up")
+                            {
+                                ProcessInput(VirtualKeyCode.VK_W);
+                            }
+                            if (message.ToLower() == "down")
+                            {
+                                ProcessInput(VirtualKeyCode.VK_S);
+                            }
+                            if (message.ToLower() == "left")
+                            {
+                                ProcessInput(VirtualKeyCode.VK_A);
+                            }
+                            if (message.ToLower() == "right")
+                            {
+                                ProcessInput(VirtualKeyCode.VK_D);
+                            }
+                            if (message.ToLower() == "a")
+                            {
+                                ProcessInput(VirtualKeyCode.VK_E);
+                            }
+                            if (message.ToLower() == "b")
+                            {
+                                ProcessInput(VirtualKeyCode.BACK);
+                            }
+                            if (message.ToLower() == "start")
+                            {
+                                ProcessInput(VirtualKeyCode.RETURN);
+                            }
+                            if (message.ToLower() == "lb")
+                            {
+                                ProcessInput(VirtualKeyCode.LEFT);
+                            }
+                            if (message.ToLower() == "rb")
+                            {
+                                ProcessInput(VirtualKeyCode.RIGHT);
+                            }
+
+                            if (message.ToLower() == "!cmd")
+                            {
+                                irc.SendChatMessage("left, right, up ,down, b, a, lb, rb, start");
+                            }
+                        }
+                    }
+                }
+                catch
+                { 
+                    
+                }
+            }
+        }
+
+        static void timer_Tick(object sender, EventArgs e)
+        {
+            irc.SendChatMessage("Commands: left, right, up ,down, b, a, lb, rb, start");
+        }
+
+        private static void ProcessInput(VirtualKeyCode Key)
+        {
+            Process p = Process.GetProcessesByName("Enché").FirstOrDefault();
+            if (p != null)
+            {
+                IntPtr h = p.MainWindowHandle;
+                SetForegroundWindow(h);
+                InputSimulator x = new InputSimulator();
+                x.Keyboard.KeyDown(Key);
+                new Task(() =>
+                {
+                    System.Threading.Thread.Sleep(100);
+                    x.Keyboard.KeyUp(Key);
+                    HasInput = false;
+                }).Start();
+            }
+        }
+    }
+}
